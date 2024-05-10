c34d471a5d829b96d19c9bec77b9182bed0e782d
Tumelo <tumeloctr23@gmail.com>
2024-05-10 14:28:00 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your name: ");
+        String name = input.next();
+        System.out.println("Welcome")+name;
         System.out.println("Hello world!");
     }
 }
